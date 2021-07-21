6a359373c6a04231940784f8796af9cc668dfbcc
Dmitriy Ignatenko <titarx@gmail.com>
2021-07-21 18:04:28 +0400

    Project commit

---
+++ MultiBatchForBzip2/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace MultiBatchForBzip2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exitButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.bzip2FileTextBox = new System.Windows.Forms.TextBox();
+            this.rootFolderTextBox = new System.Windows.Forms.TextBox();
+            this.patternTextBox = new System.Windows.Forms.TextBox();
+            this.parametersTextBox = new System.Windows.Forms.TextBox();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // exitButton
+            // 
+            this.exitButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.exitButton.Location = new System.Drawing.Point(12, 116);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.stopButton.Enabled = false;
+            this.stopButton.Location = new System.Drawing.Point(343, 116);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 2;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.startButton.Enabled = false;
+            this.startButton.Location = new System.Drawing.Point(424, 116);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 4;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // bzip2FileTextBox
+            // 
+            this.bzip2FileTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bzip2FileTextBox.ForeColor = System.Drawing.Color.Red;
+            this.bzip2FileTextBox.Location = new System.Drawing.Point(12, 12);
+            this.bzip2FileTextBox.Name = "bzip2FileTextBox";
+            this.bzip2FileTextBox.Size = new System.Drawing.Size(487, 20);
+            this.bzip2FileTextBox.TabIndex = 5;
+            this.bzip2FileTextBox.Text = "Bzip2 executable file (bzip2.exe)";
+            this.bzip2FileTextBox.Click += new System.EventHandler(this.bzip2FileTextBox_Click);
+            this.bzip2FileTextBox.TextChanged += new System.EventHandler(this.ChangingStartButtonStatus);
+            // 
+            // rootFolderTextBox
+            // 
+            this.rootFolderTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rootFolderTextBox.ForeColor = System.Drawing.Color.Red;
+            this.rootFolderTextBox.Location = new System.Drawing.Point(12, 38);
+            this.rootFolderTextBox.Name = "rootFolderTextBox";
+            this.rootFolderTextBox.Size = new System.Drawing.Size(487, 20);
+            this.rootFolderTextBox.TabIndex = 6;
+            this.rootFolderTextBox.Text = "Root folder for compression, without space (C:\\Files\\FilesForCompression)";
+            this.rootFolderTextBox.Click += new System.EventHandler(this.rootFolderTextBox_Click);
+            this.rootFolderTextBox.TextChanged += new System.EventHandler(this.ChangingStartButtonStatus);
+            // 
+            // patternTextBox
+            // 
+            this.patternTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.patternTextBox.ForeColor = System.Drawing.Color.Red;
+            this.patternTextBox.Location = new System.Drawing.Point(12, 64);
+            this.patternTextBox.Name = "patternTextBox";
+            this.patternTextBox.Size = new System.Drawing.Size(487, 20);
+            this.patternTextBox.TabIndex = 7;
+            this.patternTextBox.Text = "Pattern files for compression (*.*)";
+            this.patternTextBox.Click += new System.EventHandler(this.patternTextBox_Click);
+            this.patternTextBox.TextChanged += new System.EventHandler(this.ChangingStartButtonStatus);
+            // 
+            // parametersTextBox
+            // 
+            this.parametersTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.parametersTextBox.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.parametersTextBox.Location = new System.Drawing.Point(12, 90);
+            this.parametersTextBox.Name = "parametersTextBox";
+            this.parametersTextBox.Size = new System.Drawing.Size(487, 20);
+            this.parametersTextBox.TabIndex = 8;
+            this.parametersTextBox.Text = "Parameters separate by space (-v -9)";
+            this.parametersTextBox.Click += new System.EventHandler(this.parametersTextBox_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resetButton.Location = new System.Drawing.Point(93, 116);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 10;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar,
+            this.toolStripStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 147);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(511, 22);
+            this.statusStrip.TabIndex = 11;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar
+            // 
+            this.toolStripProgressBar.Name = "toolStripProgressBar";
+            this.toolStripProgressBar.Size = new System.Drawing.Size(100, 16);
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 169);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.parametersTextBox);
+            this.Controls.Add(this.patternTextBox);
+            this.Controls.Add(this.rootFolderTextBox);
+            this.Controls.Add(this.bzip2FileTextBox);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.exitButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MultiBatchForBzip2";
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TextBox bzip2FileTextBox;
+        private System.Windows.Forms.TextBox rootFolderTextBox;
+        private System.Windows.Forms.TextBox patternTextBox;
+        private System.Windows.Forms.TextBox parametersTextBox;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+    }
+}
+
---
+++ MultiBatchForBzip2/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using System.Diagnostics;
+
+namespace MultiBatchForBzip2
+{
+    public delegate void ChangingElementsStatusDelegate(bool stat1, bool stat2, String statusString, ProgressBarStyle progressBarStyle);
+    public delegate void UpdatingStatusLabelDelegate(String statusString);
+
+    public partial class Form1 : Form
+    {
+        private String bzip2String = "";
+        private String parametersString = "";
+        private ChangingElementsStatusDelegate changingElementsStatusDelegate = null;
+        private UpdatingStatusLabelDelegate updatingStatusLabelDelegate = null;
+        private Thread processThread = null;
+        private bool isProcessThreadJoin = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            changingElementsStatusDelegate = new ChangingElementsStatusDelegate(ChangingElementsStatus);
+            updatingStatusLabelDelegate = new UpdatingStatusLabelDelegate(UpdatingStatusLabel);
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            toolStripStatusLabel.Text = "Stoping ...";
+            if (processThread != null && processThread.IsAlive)
+            {
+                isProcessThreadJoin = true;
+                processThread.Join();
+            }
+            Application.Exit();
+        }
+
+        private void CheckEmptyFields()
+        {
+            if (bzip2FileTextBox.Text.Equals(""))
+            {
+                bzip2FileTextBox.ForeColor = Color.Red;
+                bzip2FileTextBox.Text = "Bzip2 executable file (bzip2.exe)";
+            }
+            if (rootFolderTextBox.Text.Equals(""))
+            {
+                rootFolderTextBox.ForeColor = Color.Red;
+                rootFolderTextBox.Text = @"Root folder for compression, without space (C:\Files\FilesForCompression)";
+            }
+            if (patternTextBox.Text.Equals(""))
+            {
+                patternTextBox.ForeColor = Color.Red;
+                patternTextBox.Text = "Pattern files for compression (*.*)";
+            }
+            if (parametersTextBox.Text.Equals(""))
+            {
+                parametersTextBox.ForeColor = SystemColors.GrayText;
+                parametersTextBox.Text = "Parameters separate by space (-v -9)";
+            }
+        }
+
+        private void StartProcess()
+        {
+            try
+            {
+                bzip2String = bzip2FileTextBox.Text.Trim();
+                String rootFolderString = rootFolderTextBox.Text.Trim();
+                parametersString = parametersTextBox.Text.Trim();
+                if (parametersString.Equals("Parameters separate by space (-v -9)"))
+                {
+                    parametersString = "";
+                }
+                parametersString = String.Format("{0} {1}", parametersString, patternTextBox.Text.Trim()).Trim();
+
+                if (!File.Exists(bzip2String) || !new FileInfo(bzip2String).Extension.Equals(".exe"))
+                {
+                    MessageBox.Show("Bzip2 file not found or has an incorrect format", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else if (!Directory.Exists(rootFolderString))
+                {
+                    MessageBox.Show("Root folder for compression not found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    Process(rootFolderString);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                //Без условия - взаимная блокировка, при "processThread.Join()" в главном потоке
+                if (!isProcessThreadJoin)
+                {
+                    this.Invoke(changingElementsStatusDelegate, new object[] { false, true, "Done", ProgressBarStyle.Blocks });
+                }
+            }
+        }
+
+        private void Process(String folderString)
+        {
+            this.Invoke(updatingStatusLabelDelegate, new object[] { folderString });
+
+            Process process = new Process();
+            process.StartInfo.FileName = bzip2String;
+            process.StartInfo.WorkingDirectory = folderString;
+            process.StartInfo.Arguments = parametersString;
+            process.Start();
+            process.WaitForExit();
+
+            foreach (DirectoryInfo directoryInfo in new DirectoryInfo(folderString).GetDirectories())
+            {
+                if (Directory.Exists(directoryInfo.FullName))
+                {
+                    if (!isProcessThreadJoin)
+                    {
+                        Process(directoryInfo.FullName);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void ChangingElementsStatus(bool stat1, bool stat2, String statusString, ProgressBarStyle progressBarStyle)
+        {
+            bzip2FileTextBox.Enabled = stat2;
+            rootFolderTextBox.Enabled = stat2;
+            patternTextBox.Enabled = stat2;
+            parametersTextBox.Enabled = stat2;
+            resetButton.Enabled = stat2;
+            startButton.Enabled = stat2;
+            stopButton.Enabled = stat1;
+            toolStripProgressBar.Style = progressBarStyle;
+            toolStripStatusLabel.Text = statusString;
+        }
+
+        private void UpdatingStatusLabel(String statusString)
+        {
+            toolStripStatusLabel.Text = statusString;
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            bzip2FileTextBox.ForeColor = Color.Red;
+            bzip2FileTextBox.Text = "Bzip2 executable file (bzip2.exe)";
+            rootFolderTextBox.ForeColor = Color.Red;
+            rootFolderTextBox.Text = @"Root folder for compression, without space (C:\Files\FilesForCompression)";
+            patternTextBox.ForeColor = Color.Red;
+            patternTextBox.Text = "Pattern files for compression (*.*)";
+            parametersTextBox.ForeColor = SystemColors.GrayText;
+            parametersTextBox.Text = "Parameters separate by space (-v -9)";
+            toolStripProgressBar.Style = ProgressBarStyle.Blocks;
+            toolStripStatusLabel.Text = "";
+            startButton.Enabled = false;
+            stopButton.Enabled = false;
+        }
+
+        private void bzip2FileTextBox_Click(object sender, EventArgs e)
+        {
+            CheckEmptyFields();
+            if (bzip2FileTextBox.Text.Equals("Bzip2 executable file (bzip2.exe)"))
+            {
+                bzip2FileTextBox.ForeColor = SystemColors.WindowText;
+                bzip2FileTextBox.Text = "";
+            }
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = ".";
+            openFileDialog.Filter = "*.exe|*.exe";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                bzip2FileTextBox.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void rootFolderTextBox_Click(object sender, EventArgs e)
+        {
+            CheckEmptyFields();
+            if (rootFolderTextBox.Text.Equals(@"Root folder for compression, without space (C:\Files\FilesForCompression)"))
+            {
+                rootFolderTextBox.ForeColor = SystemColors.WindowText;
+                rootFolderTextBox.Text = "";
+            }
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                rootFolderTextBox.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void patternTextBox_Click(object sender, EventArgs e)
+        {
+            CheckEmptyFields();
+            if (patternTextBox.Text.Equals("Pattern files for compression (*.*)"))
+            {
+                patternTextBox.ForeColor = SystemColors.WindowText;
+                patternTextBox.Text = "";
+            }
+        }
+
+        private void parametersTextBox_Click(object sender, EventArgs e)
+        {
+            CheckEmptyFields();
+            if (parametersTextBox.Text.Equals("Parameters separate by space (-v -9)"))
+            {
+                parametersTextBox.ForeColor = SystemColors.WindowText;
+                parametersTextBox.Text = "";
+            }
+        }
+
+        private void ChangingStartButtonStatus(object sender, EventArgs e)
+        {
+            if (bzip2FileTextBox.Text.Equals("") || bzip2FileTextBox.Text.Equals("Bzip2 executable file (bzip2.exe)") ||
+                rootFolderTextBox.Text.Equals("") || rootFolderTextBox.Text.Equals(@"Root folder for compression, without space (C:\Files\FilesForCompression)") ||
+                patternTextBox.Text.Equals("") || patternTextBox.Text.Equals("Pattern files for compression (*.*)"))
+            {
+                startButton.Enabled = false;
+            }
+            else
+            {
+                startButton.Enabled = true;
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            processThread = new Thread(new ThreadStart(StartProcess));
+            processThread.Start();
+            ChangingElementsStatus(true, false, "Starting ...", ProgressBarStyle.Marquee);
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            toolStripStatusLabel.Text = "Stoping ...";
+            if (processThread != null && processThread.IsAlive)
+            {
+                isProcessThreadJoin = true;
+                processThread.Join();
+            }
+            ChangingElementsStatus(false, true, "Stoped", ProgressBarStyle.Blocks);
+            isProcessThreadJoin = false;
+        }
+    }
+}
